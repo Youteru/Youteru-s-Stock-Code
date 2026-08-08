adf50d8d0264b7e461e51ed8bc82fce720368b04
Youteru <uno.teruki@gmail.com>
2026-08-08 14:01:39 +0900

    Update 偏角ソート.py

--- すとっくこーど/座標・平面幾何学/偏角ソート.py
+++ すとっくこーど/座標・平面幾何学/偏角ソート.py
@@ -1,13 +1,27 @@
 from functools import cmp_to_key
-
-
-def arg_sort(p0, p1):
-    x0, y0 = p0
-    x1, y1 = p1
-    if x0 * y1 == x1 * y0:
+import math
+def Syogen(p) :
+    #pは2引数タプル
+    if p[0]>=0 and p[1]>=0 :
+        return 1
+    elif p[0]<0 and p[1]>=0 :
+        return 2
+    elif  p[0]<0 and p[1]<0 :
+        return 3
+    else :
+        return 4
+def angle_cmp(p1, p2):
+    if Syogen(p1)<Syogen(p2) :
+        return -1
+    if Syogen(p2)<Syogen(p1) :
+        return 1
+    cross_product = p1[0] * p2[1] - p1[1] * p2[0]
+    if cross_product > 0 :
+        return -1
+    elif cross_product < 0 :
+        return 1
+    else :
         return 0
-    return 1 if x0 * y1 < x1 * y0 else -1
-
 
 N = int(input())
 lst = []
